859064129113437d75b3e07b45cc37a4f4b61bb3
choward3413 <106481108+choward3413@users.noreply.github.com>
2022-07-26 23:57:59 -0400

    Update api_keys.py

--- Module 06 Challenge - Due 07-26-2022/starter_code/api_keys.py
+++ Module 06 Challenge - Due 07-26-2022/starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "6e4e6520729a06888e3f236ac2203385"
+weather_api_key = ""
 
 # Google API Key
-g_key = "AIzaSyDvyVokQgmlv9s92X1Bh5cxtgnninMiArk"
+g_key = ""
